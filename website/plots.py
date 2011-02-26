ffc2f6217486d25b803c73cd09948b85a27d1305
Rene Milk <rene.milk@uni-muenster.de>
2011-02-27 00:57:08 +0100

    make plot date offset common global

--- website/plots.py
+++ website/plots.py
@@ -10,17 +10,19 @@ from db_entities import Match,Player,Result
 from threading import Lock
 mutex = Lock()
 
+fail_offset = timedelta(days=363)
+
 @cache.cache('matches_per_ladder',expire=3600)
 def matches_per_ladder( ladderid ):
 	print 'not cached: matches_per_ladder( %s )'% ladderid
 	s = db.sessionmaker()
 	inc = timedelta(days=1)
 	today = datetime.combine(date.today(), time.min ) #datetime( now.year, now.month, now.day )
-	since = today - timedelta(days=7) - timedelta(days=363)
+	since = today - timedelta(days=7) - fail_offset
 	now = since
 	data = []
 	i = 1
-	while now < datetime.now() - timedelta(days=363):
+	while now < datetime.now() - fail_offset:
 		data.append( s.query( Match.id ).filter( Match.ladder_id == ladderid).filter( Match.date < now + inc ).filter( Match.date >= now ).count() )
 		i += 1
 		now += inc
@@ -41,11 +43,11 @@ def matches_per_player( playerid ):
 	s = db.sessionmaker()
 	inc = timedelta(days=1)
 	today = datetime.combine(date.today(), time.min ) #datetime( now.year, now.month, now.day )
-	since = today - timedelta(days=7) - timedelta(days=363)
+	since = today - timedelta(days=7) - fail_offset
 	now = since
 	data = []
 	i = 1
-	while now < datetime.now() - timedelta(days=363):
+	while now < datetime.now() - fail_offset:
 		data.append( s.query( Result.id ).filter( Result.player_id == playerid).filter( Result.date < now + inc ).filter( Result.date >= now ).count() )
 		i += 1
 		now += inc
